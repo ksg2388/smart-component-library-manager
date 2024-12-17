673535f8e857667c6b082926027750e2b9e2468d
ksg2388 <ksg2388@naver.com>
2024-12-17 15:01:35 +0900

    Fix: 레이아웃 수정

--- src/app/_components/common/manage/ResponsiveSlider.tsx
+++ src/app/_components/common/manage/ResponsiveSlider.tsx
@@ -1,28 +1,36 @@
 "use client";
 
-// Carousel.tsx
-import React from "react";
+import React, { useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Image from "next/image";
 
 type CarouselProps = {
-  items: string[]; // 캐러셀에 표시할 아이템 (문자열 배열)
+  items: string[];
 };
 
 const Carousel: React.FC<CarouselProps> = ({ items }) => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [slideCount, setSlideCount] = useState(items.length);
+
   const settings = {
-    dots: true, // 하단 네비게이션 점 표시
-    infinite: true, // 무한 스크롤
-    speed: 500, // 슬라이드 속도 (ms)
-    slidesToShow: 3, // 한 번에 보여줄 슬라이드 수
-    slidesToScroll: 1, // 한 번에 스크롤할 슬라이드 수
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    beforeChange: (current: number, next: number) => {
+      setCurrentSlide(next);
+    },
+    afterChange: (current: number) => {
+      setCurrentSlide(current);
+    },
     responsive: [
       {
-        breakpoint: 768, // 반응형: 화면 너비가 768px 이하일 때
+        breakpoint: 768,
         settings: {
-          slidesToShow: 1, // 한 번에 1개 표시
+          slidesToShow: 1,
           slidesToScroll: 1,
         },
       },
@@ -34,7 +42,6 @@ const Carousel: React.FC<CarouselProps> = ({ items }) => {
       <Slider {...settings}>
         {items.map((item, index) => (
           <div key={index} className="px-2 pt-7">
-            {/* 간격을 위한 padding */}
             <div className="text-center">{item}</div>
             <div className="flex items-center justify-center h-[170px] rounded-lg">
               <Image
@@ -47,6 +54,13 @@ const Carousel: React.FC<CarouselProps> = ({ items }) => {
           </div>
         ))}
       </Slider>
+      <style jsx global>{`
+        /* 화살표 스타일링 */
+        .slick-prev,
+        .slick-next {
+          margin-top: 25px; /* 화살표를 아래로 12px 이동 */
+        }
+      `}</style>
     </div>
   );
 };
